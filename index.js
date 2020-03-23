ca7d0e6fc36b8173dd550f94c7151c2dd5a5492d
David Brookes <130948+dbrookes@users.noreply.github.com>
2020-03-23 00:43:36 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 export { default as ErrorBoundary } from "./components/ErrorBoundary"
-
-export { default as BaseAlert } from "./components/Alert"
-
+export { default as Alert } from "./components/Alert"
 export { default as Icon } from "./components/Icon"
 export { default as SocialIcon } from "./components/SocialIcon"
-
 export { default as Form } from "./components/Form"
-export { FormsyInput as BaseInput } from "./components/Input"
-export { FormsyCheckbox as BaseCheckbox } from "./components/Checkbox"
-export { FormsySelect as BaseSelect } from "./components/Select"
-export { FormsyTextarea as BaseTextArea } from "./components/Textarea"
-
+export { FormsyInput as Input } from "./components/Input"
+export { FormsyCheckbox as Checkbox } from "./components/Checkbox"
+export { FormsySelect as Select } from "./components/Select"
+export { FormsyTextarea as Textarea } from "./components/Textarea"
 export { default as Button } from "./components/Button"
 export { default as ButtonTo } from "./components/ButtonTo"
